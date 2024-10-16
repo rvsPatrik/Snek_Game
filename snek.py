938e071b6866c2db4ba5f89fc173333d775db132
Patrik Revesz <rvs.patrik.2003@gmail.com>
2024-10-16 23:16:00 +0200

    working-ish snek commit

--- snek.py
+++ snek.py
@@ -1,26 +1,45 @@
 #!/usr/bin/env python3
 
 import pygame
+import random
 import time
 
-SCREEN_WIDTH = 700
+pygame.init()
+
+SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 700
-ITEM_SIZE = 25
-
-SCREEN_COLOR = "Black"
-FOOD_COLOR = "White"
-SNEK_COLOR = "Green"
-
-snake_size = 10
-snake_speed = 15
-snake_body = [SCREEN_WIDTH//2, SCREEN_HEIGHT//2]
-
-x,y = 0,0
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('SNEK')
 
+SCREEN_COLOR = (0,0,0)
+FOOD_COLOR = (255,255,255)
+SNEK_COLOR = (0,255,0)
+GO_TEXT = (255,0,0)
+
 clock = pygame.time.Clock()
+
+snake_size = 20
+snake_speed = 30
+snake_body = [(SCREEN_WIDTH//2, SCREEN_HEIGHT//2)]
+
+x,y = 0,0
+
+
+
+def generate_food():
+    return ( random.randint(0,(SCREEN_WIDTH-snake_size) // snake_size) * snake_size,random.randint(0,(SCREEN_HEIGHT-snake_size) // snake_size)*snake_size)
+food_pos = generate_food()
+
+
+font = pygame.font.SysFont('arialblack',55)
+
+def game_over_msg():
+    message = font.render('Game Over Browski',True,GO_TEXT)
+    screen.blit(message,[SCREEN_WIDTH//4,SCREEN_HEIGHT//4])
+    pygame.display.update()
+    time.sleep(2)
+
 
 running = True
 
@@ -39,16 +58,34 @@ while running:
             elif event.key == pygame.K_DOWN:
                 x,y = 0,snake_size
 
-    head = (snake_body[0][0])
+
+    head = (snake_body[0][0] + x, snake_body[0][1] + y)
     snake_body.insert(0,head)
     snake_body.pop()
 
+    if head[0] < 0 or head[0] >= SCREEN_WIDTH or head[1] < 0 or head[1] >= SCREEN_HEIGHT:
+        running = False
+
+    if snake_body[0] == food_pos:
+        food_pos = generate_food()
+        snake_body.append(snake_body[-1])
+
     screen.fill(SCREEN_COLOR)
 
+
+    
     for segment in snake_body:
         pygame.draw.rect(screen,SNEK_COLOR,pygame.Rect(segment[0],segment[1],snake_size,snake_size))
+    
+    pygame.draw.rect(screen,FOOD_COLOR,pygame.Rect(food_pos[0], food_pos[1],snake_size,snake_size))
+
 
     pygame.display.update()
-    clock.tick(30)
+    clock.tick(snake_speed)
 
+
+
+
+
+game_over_msg()
 pygame.quit()
