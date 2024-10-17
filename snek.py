62d6a7b1001984b914a3b205b44da8c78db89c10
Patrik Revesz <rvs.patrik.2003@gmail.com>
2024-10-17 09:42:10 +0200

    even more scuffed snek

--- snek.py
+++ snek.py
@@ -16,19 +16,20 @@ SCREEN_COLOR = (0,0,0)
 FOOD_COLOR = (255,255,255)
 SNEK_COLOR = (0,255,0)
 GO_TEXT = (255,0,0)
-
+ASD = 20
 clock = pygame.time.Clock()
 
-snake_size = 10
+item_size = 10
 snake_speed = 10
 snake_body = [(SCREEN_WIDTH//2, SCREEN_HEIGHT//2)]
+snake_direction = None
 
 x,y = 0,0
 
 
 
 def generate_food():
-    return ( random.randint(0,(SCREEN_WIDTH-snake_size) // snake_size) * snake_size,random.randint(0,(SCREEN_HEIGHT-snake_size) // snake_size)*snake_size)
+    return ( random.randint(0,(SCREEN_WIDTH-item_size) // item_size) * item_size,random.randint(0,(SCREEN_HEIGHT-item_size) // item_size)*item_size)
 food_pos = generate_food()
 
 
@@ -44,19 +45,27 @@ def game_over_msg():
 running = True
 
 while running:
+    snake_direction = pygame.Vector2(0,0)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+
+
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x,y = -snake_size,0
-            elif event.key == pygame.K_RIGHT:
-                x,y = snake_size,0
-            elif event.key == pygame.K_UP:
-                x,y = 0,-snake_size
-            elif event.key == pygame.K_DOWN:
-                x,y = 0,snake_size
+            if event.key == pygame.K_LEFT and not snake_direction[0]>0:
+                x,y = -item_size,0
+                snake_direction = pygame.Vector2(-ASD,0)
+            if event.key == pygame.K_RIGHT and not snake_direction[0]<0:
+                x,y = item_size,0
+                snake_direction = pygame.Vector2(ASD,0)
+            if event.key == pygame.K_UP and not snake_direction[1]>0:
+                x,y = 0,-item_size
+                snake_direction = pygame.Vector2(0.-ASD)
+            if event.key == pygame.K_DOWN and not snake_direction[1]<0:
+                x,y = 0,item_size
+                snake_direction = pygame.Vector2(0,ASD)
 
 
     head = (snake_body[0][0] + x, snake_body[0][1] + y)
@@ -73,12 +82,14 @@ while running:
 
     screen.fill(SCREEN_COLOR)
 
-
+    for i in range (0, (SCREEN_HEIGHT+SCREEN_WIDTH)//2,item_size):
+       # pygame.draw.line(screen,SCREEN_COLOR,(i,0),(i))
+       pass
     
     for segment in snake_body:
-        pygame.draw.rect(screen,SNEK_COLOR,pygame.Rect(segment[0],segment[1],snake_size,snake_size))
+        pygame.draw.rect(screen,SNEK_COLOR,pygame.Rect(segment[0],segment[1],item_size,item_size))
     
-    pygame.draw.rect(screen,FOOD_COLOR,pygame.Rect(food_pos[0], food_pos[1],snake_size,snake_size))
+    pygame.draw.rect(screen,FOOD_COLOR,pygame.Rect(food_pos[0], food_pos[1],item_size,item_size))
 
 
     pygame.display.update()
